01a0d6add5f7b6d9093db440aab0d92b1a669713
Riazul Islam <ri64627@gmail.com>
2018-01-14 10:14:26 +0600

    fff

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,10 +7,30 @@
             items:1,
             loop:true,
             nav:true,
-            dots:false,
-            navText: ["<i class='fa fa-long-arrow-left'>","<i class='fa fa-long-arrow-right'>"],
+            dots:true,
+            navText: ["<i class='fa fa-angle-left'>","<i class='fa fa-angle-right'>"],
             autoplay:true,
         });
+
+         //Hamburger Menu
+        $(".menu-triger").on("click", function() {
+            $(".offcanvas-menu , .offcanvas-overlay").addClass("active");
+            return false;
+        });
+
+        $(".menu-close , .offcanvas-overlay").on("click", function() {
+            $(".offcanvas-menu ,.offcanvas-overlay").removeClass("active");
+        });
+
+        $(window).scroll(function(){
+            if($(this).scrollTop()>100){
+                $(".header").addClass("sticky");
+            }
+            else{
+                $(".header").removeClass("sticky");
+            }
+        });
+
 
     });
 
